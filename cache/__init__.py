864ecb9a6507e4a89aca051d9a6946ffdd37dbd1
Reza Mirabizadeh <remi.ir@gmail.com>
2021-01-18 00:58:56 +0330

    init web drivers folder

--- cache/__init__.py
+++ cache/__init__.py
@@ -5,6 +5,9 @@ from logging.handlers import TimedRotatingFileHandler
 
 web_drivers_dir_path = os.path.join(".", "cache", "drivers")
 
+if not os.path.exists(web_drivers_dir_path):
+    os.mkdir(web_drivers_dir_path)
+
 drivers = {}
 for _, _, _file in os.walk(web_drivers_dir_path):
     for f in sorted(_file):
@@ -12,10 +15,14 @@ for _, _, _file in os.walk(web_drivers_dir_path):
 
 logger = logging.getLogger('main_logger')
 logger.setLevel(settings.LOG_LEVEL)
-formatter = logging.Formatter(fmt='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-                            datefmt='%m-%d-%y %H:%M:%S')
+formatter = logging.Formatter(
+    fmt='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+    datefmt='%m-%d-%y %H:%M:%S'
+)
+
 if not os.path.exists("./logs"):
     os.mkdir("./logs")
+
 fh = TimedRotatingFileHandler('./logs/main_logger.log', when='D', interval=1)
 fh.setFormatter(formatter)
 logger.addHandler(fh)
