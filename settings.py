a4bd1d3a88918a8bc78307030571104d6ca3d86b
Reza Mirabizadeh <remi.ir@gmail.com>
2021-01-18 10:37:27 +0330

    init general settings

---
+++ settings.py
@@ -0,0 +1,14 @@
+import logging
+# TODO remove unnecessary params
+
+# Set logging enable or disable
+LOG = True
+# Log level
+LOG_LEVEL = logging.DEBUG
+# API host
+HOST = "0.0.0.0"
+# API port
+PORT = 6000
+# TODO Set this variables into right state for production ! IMPORTANT
+DEBUG = True
+PRODUCTION = True
