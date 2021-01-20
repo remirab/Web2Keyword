dedc71340255c88397f6c19b66eab2ca2628cd72
Reza Mirabizadeh <remi.ir@gmail.com>
2021-01-20 18:11:11 +0400

    add workers timeout default time in seconds

--- settings.py
+++ settings.py
@@ -3,12 +3,13 @@ import logging
 
 # Set logging enable or disable
 LOG = True
-# Log level
 LOG_LEVEL = logging.DEBUG
-# API host
+
+# API host and port and workers timeout in seconds
 HOST = "127.0.0.1"
-# API port
 PORT = 8080
+W_TIMEOUT = 60
+
 # TODO Set this variables into right state for production ! IMPORTANT
 DEBUG = True
 PRODUCTION = True
